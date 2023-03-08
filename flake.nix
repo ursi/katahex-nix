62539684466c5f5a3c06c90acf74f8dbe3e72e85
Mason Mackaman <masondeanm@aol.com>
2023-03-07 22:35:23 -0500

    Revert "Move source fetching to flake inputs"
    This causes the google drive sources to be redownloaded for different
    shells. Not sure if this is intended behaviour or not, but regardless,
    it's a better UX without this change.
    
    This reverts commit 954d1ba9cd08aefc97c4715764a71976d33e6ed1.

--- flake.nix
+++ flake.nix
@@ -1,8 +1,6 @@
 { inputs =
     { katago = { url = "github:hzyhhzy/KataGo/Hex2022"; flake = false; };
-      lizzyzy = { url = "https://drive.google.com/u/0/uc?id=1qbTTmPFiUkM_346DeKS1E9gJR-roNH63&export=download&confirm=t&uuid=101373af-a46f-4794-a1c7-151c1f5fe59a&at=ALgDtszvi-tg4S5aGQ9t7AnXcflU:1676397446078"; flake = false; };
       make-shell.url = "github:ursi/nix-make-shell/1";
-      model = { url = "https://drive.google.com/u/0/uc?id=1xMvP_75xgo0271nQbmlAJ40rvpKiFTgP&export=download&confirm=t&uuid=0fd3460e-dc4e-4e7d-81a5-ab11c1cf1772&at=ALgDtswWGdmlMCoyYDj55UXZkQQ5:1676420358456"; flake = false; };
       nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
       utils.url = "github:ursi/flake-utils/8";
     };
@@ -31,9 +29,16 @@
              )).override { enableGPU = false; };
 
            lizzieyzy =
+             let
+               src =
+                 p.fetchurl
+                   { url = "https://drive.google.com/u/0/uc?id=1qbTTmPFiUkM_346DeKS1E9gJR-roNH63&export=download&confirm=t&uuid=101373af-a46f-4794-a1c7-151c1f5fe59a&at=ALgDtszvi-tg4S5aGQ9t7AnXcflU:1676397446078";
+                     hash = "sha256-IfvKChZVaxdPrmm2NTAzS19+VW2IQ8gK5+UG0rKFESk=";
+                   };
+             in
              p.runCommand "lizzieyzy" {}
                ''
-               ${p.unzip}/bin/unzip ${inputs.lizzyzy}
+               ${p.unzip}/bin/unzip ${src}
                mv KataHex_LizzieYZY $out
                cd $out
                patch config.txt ${./config.patch}
@@ -41,6 +46,13 @@
                find -name '*.dll' -exec rm "{}" ";"
                find -name '*.exe' -exec rm "{}" ";"
                '';
+
+           model =
+             p.fetchurl
+               { url = "https://drive.google.com/u/0/uc?id=1xMvP_75xgo0271nQbmlAJ40rvpKiFTgP&export=download&confirm=t&uuid=0fd3460e-dc4e-4e7d-81a5-ab11c1cf1772&at=ALgDtswWGdmlMCoyYDj55UXZkQQ5:1676420358456";
+                 hash = "sha256-DWvLu0Upd49uGLI0t7ftxWjjzfiwjqfrDzXmXofvFwU=";
+               };
+
 
            shell = board-size:
              make-shell
@@ -74,7 +86,7 @@
                    ''
                    cp -nr ${lizzieyzy}/. .
                    chmod -R u+w .
-                   ln -fs ${inputs.model} weights/katahex_model_20220618.bin.gz
+                   ln -fs ${model} weights/katahex_model_20220618.bin.gz
                    ln -fs ${katago board-size}/bin/katago engine/katahex${toString board-size}
                    chmod -R u+w .
 
